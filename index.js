f010d4b662aeecdaaebe7ad9c7cfb7c605f9a6e0
Devin Mullin <devin.mullin90@gmail.com>
2021-08-31 08:57:09 -0500

    commit tree ed4380a982368854b3966a39f1f554f657e279c3

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code in this file!
+let currentUser = 'Devin'
+let upperCaseCurrentUser = currentUser.toUpperCase()
+const firstInitial = currentUser[0]
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`
+let shortGreeting = `Welcome, ${firstInitial}!`
